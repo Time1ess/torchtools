b00f1a3f1c88e42779b237471bc12e20c214b315
David <youchen.du@gmail.com>
2017-09-11 15:02:35 +0800

    [fix] Meter in __all__.

--- torchtools/meters/__init__.py
+++ torchtools/meters/__init__.py
@@ -3,16 +3,18 @@
 # Author: David
 # Email: youchen.du@gmail.com
 # Created: 2017-08-14 21:20
-# Last modified: 2017-09-11 14:42
+# Last modified: 2017-09-11 15:02
 # Filename: __init__.py
 # Description:
+from .meter import Meter
 from .lossmeter import EpochLossMeter, BatchLossMeter
 from .timemeter import TimeMeter
 from .ioumeter import IoUMeter, EpochIoUMeter, BatchIoUMeter
 from .semanticmeter import SemSegVisualizer
 
 
-__all__ = ['EpochLossMeter', 'BatchLossMeter',
+__all__ = ['Meter',
+           'EpochLossMeter', 'BatchLossMeter',
            'TimeMeter',
            'IoUMeter', 'EpochIoUMeter', 'BatchIoUMeter',
            'SemSegVisualizer']
